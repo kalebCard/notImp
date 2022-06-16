76b3187b535343de30b3f891e830317426902b89
Kaleb_Cardona <107603014+kalebCard@users.noreply.github.com>
2022-06-16 18:58:15 -0500

    Update newFile.py

--- newFile.py
+++ newFile.py
@@ -6,5 +6,15 @@ url = https://www.metacritic.com/browse/games/score/metascore/year/all/filtered
 page = request.get (url)
 soup = Beautifulsoup(page.content, 'html.parser')
 
-#Equipo
+#Equipos
 
+eq= soup.find_all('a', class_="title")
+
+
+juegos = list()
+
+for it in eq:
+  equipos.append(i.text)
+  
+  print(equipos)
+  
